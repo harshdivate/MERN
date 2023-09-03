00353ee41492603d70a37525fb87a47d3ee998b0
Harsh <harshdivate45@gmail.com>
2023-09-03 14:46:07 +0530

    Lesson 2 Completed

---
+++ ReactJs/01/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Header from './Header';
+import Content from './content';
+import Footer from './footer';
+
+function App() {
+  
+  return (
+    <div className="App">
+      <Header/>
+      <Content/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
